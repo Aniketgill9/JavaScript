eda88764c69f9d5e4134a0e2472ae39884f566e6
Aniket gill <gillaniket2002@gmail.com>
2025-07-09 21:02:22 +0530

    Add memory type explanations and update variable names in datatype summary

--- 05_datatype_summary.js
+++ 05_datatype_summary.js
@@ -15,7 +15,17 @@
 // 3. function
 
 
+// ********************************************************************************
 
 
+    // memory
+    // stack  (stack is used for primitive data types), 
+    // heap (heap is used for non-primitive data types)
+     let myYoutubename ="aniket"
 
-    
+     let anothername = myYoutubename
+     anothername = "aniket gill"
+
+     console.log(myYoutubename);
+     console.log(anothername);
+     
