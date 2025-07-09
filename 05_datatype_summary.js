a983b7c4c2c1221a411f91ea7f55f7c99f5191e1
Aniket gill <gillaniket2002@gmail.com>
2025-07-09 09:55:52 +0530

    First commit

---
+++ 05_datatype_summary.js
@@ -0,0 +1,21 @@
+// primitive data types
+// 1. string        
+// 2. number
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. symbol
+// 7. bigint
+
+
+// non-primitive data types/reference data types
+
+// 1. array 
+// 2. object
+// 3. function
+
+
+
+
+
+    
