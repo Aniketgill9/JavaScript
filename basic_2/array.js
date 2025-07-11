cbfe05cd88692d7c59525f0f60e9b333c1f2408f
Aniket gill <gillaniket2002@gmail.com>
2025-07-11 21:25:30 +0530

    Add initial array.js file with commented examples and array methods

---
+++ basic_2/array.js
@@ -0,0 +1,28 @@
+// const myArray =[1,2,3,4,5,]
+// const Herro =["aniket","sachin","sagar","pratik"]
+// console.log(myArray[3]); // Accessing the first element of the array
+// console.log(Herro[2]); // Accessing the third element of the array
+
+// const myArray2 = new Array(1,2,3,4,5,6,7,8,9,10)
+// console.log(myArray2); // Output: [1, 2, 3, 4
+
+// console.log(myArray2.length); // Output the length of the array
+
+  
+
+// array methods
+
+// const myArr =[2,4,7,9,"aniket"]
+
+// myArr.push("gill")
+// myArr.pop() // removes the last element
+
+// myArr.unshift("hello") // adds an element at the beginning
+//  myArr.shift("tata") // removes the first element
+// console.log(myArr); 
+
+
+
+// slice,splice
+
+const myArr =[2,4,7,9,"aniket"]
