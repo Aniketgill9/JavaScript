ef08efc31150569116d6d43add371c931cd68335
Aniket gill <gillaniket2002@gmail.com>
2025-07-11 15:20:38 +0530

    Add string manipulation examples and demonstrate string interpolation

---
+++ 06_string.js
@@ -0,0 +1,31 @@
+const name ="aniket"
+const age = 20;
+//console.log(name+" is "+age+" years old ");
+console.log((`my name is ${name} and my age is ${ age}`))  //new version of string that is known as string interpolation
+
+
+const gameName = "CRICKET"
+console.log(gameName[0],gameName[4]);
+console.log(gameName.length); //length of string
+console.log(gameName.indexOf("C"));//index of string
+console.log(gameName.replace("CRICKET","FOOTBALL")); //replace the string
+console.log(gameName.toLowerCase()); //convert to lower case
+console.log(gameName.toUpperCase()); //convert to upper case
+console.log(gameName.slice(0,4));//slice the string from index 1 to 3
+console.log(gameName.split("C"));//split the string by "C"
+console.log(gameName.charAt(6)); //get character at index 6
+console.log(gameName.endsWith("T")); //check if string ends with "T"
+console.log(gameName.endsWith("f")); //check if string ends with "T"
+console.log(gameName.startsWith("C")); //check if string starts with "C"
+console.log(gameName.startsWith("k")); //check if string starts with "C"
+
+
+
+
+
+
+
+
+
+
+
