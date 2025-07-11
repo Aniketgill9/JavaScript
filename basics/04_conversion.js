0e3a0c99c00d6d1682ed0b188f0814514ed75ed3
Aniket gill <gillaniket2002@gmail.com>
2025-07-11 19:57:43 +0530

    Refactor: Remove outdated variable and datatype examples across multiple files

---
+++ basics/04_conversion.js
@@ -0,0 +1,33 @@
+// why string to number conversion is confusing?
+//  operations
+// let val = 3
+// let negval = -val
+// console.log(val)
+
+
+// let str1 ="hello "
+// let str2 = "aniket"
+// let str3 = str1 + str2; 
+// console.log(str3);
+
+
+//camparison of datatype  / comparison and equality check are different thing
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// === is strict equality check  example:-
+console.log("2"===2); // false
+console.log("2"== 2); // true
+
+
+
+ 
+
+
+
+
