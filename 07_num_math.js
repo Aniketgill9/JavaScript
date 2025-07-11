91a877cba1092e807bd7dec6e8d614f2fd3ec9fe
Aniket gill <gillaniket2002@gmail.com>
2025-07-11 15:20:52 +0530

    Add number manipulation examples with console outputs

---
+++ 07_num_math.js
@@ -0,0 +1,11 @@
+
+const score =100
+console.log(score);
+const number = new Number (100);
+console.log(number);
+  console.log(number.toString().length)
+  console.log(number.toFixed(3)) //convert to string and fix to 3 decimal places
+
+  const otherNumber = 125.79664;
+  console.log(otherNumber.toPrecision(3)) //convert to string and fix to 3 decimal places;
+  
